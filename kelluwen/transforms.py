ea3f6bd2b1106f4ac8a6a483c741bbd7a2f73c02
Felipe Moser <felipe.moser@univ.ox.ac.uk>
2021-08-22 15:30:01 +0100

    Added parameters_from_homogeneous_affine_3d

--- kelluwen/transforms.py
+++ kelluwen/transforms.py
@@ -18,7 +18,7 @@ class AffineTransformer3d(torch.nn.Module):
         rotation : torch.Tensor
             rotation angles for affine transform matrix, only Euler angles currently supported
         translation : torch.Tensor
-            translation parameters for affine transform matrix, normalised to [-1, 1]
+            translation parameters for affine transform matrix, normalised to [-1, 1] where 1 is half the size of input, i.e. for a 160x160x160 input, [-1, 1] represents [-80, 80].
         scaling : torch.Tensor
             scaling parameters for affine transform matrix
 
@@ -98,7 +98,7 @@ def affine_transform_3d(rotation, translation, scaling):
         raise TypeError("scaling must be a tensor")
     elif scaling.dim() != 2 or scaling.shape[1] != 3:
         raise ValueError("scaling must be an Nx3 tensor")
-
+    
     # Calculate rotation transform
     R = rotation_transform_3d(rotation)
 
@@ -215,3 +215,48 @@ def scaling_transform_3d(scaling):
     S[:, 2, 2] = scaling[:, 2]
 
     return S
+
+
+def parameters_from_homogeneous_affine_3d(transform):
+    """Returns rotation parameters, translation parameters, and scaling parameters from a homogeneous 3D affine transform matrix . Only Euler angles currently supported for the rotation parameters.
+
+    Parameters
+    ----------
+    transform : torch.Tensor
+        homogeneous 3D affine transform matrix
+
+    Returns
+    -------
+    torch.Tensor
+        3D rotation parameters (Euler angles)
+    torch.Tensor
+        3D translation parameters
+    torch.Tensor
+        3D scaling parameters
+    """
+    # Check transform parameter
+    if not isinstance(transform, torch.Tensor):
+        raise TypeError("transform must be a tensor")
+    elif transform.dim() != 3 or transform.shape[1] != 4 or transform.shape[2] != 4:
+        raise ValueError("transform must be of shape Nx4x4")
+
+    # Calculate scaling parameters
+    Sx = torch.linalg.norm(transform[:, :-1, 0], dim=1)
+    Sy = torch.linalg.norm(transform[:, :-1, 1], dim=1)
+    Sz = torch.linalg.norm(transform[:, :-1, 2], dim=1)
+    scaling_params = torch.stack([Sx, Sy, Sz], dim=1)
+
+    # Calculate translation parameters
+    Tx = transform[:, 0, 3]
+    Ty = transform[:, 1, 3]
+    Tz = transform[:, 2, 3]
+    translation_params = torch.stack([Tx, Ty, Tz], dim=1)
+
+    # Calculate rotation parameters (Euler angles)
+    Rx = torch.atan2(-transform[:, 1, 0], transform[:, 0, 0])
+    Ry = torch.asin(transform[:, 2, 0] / Sx)
+    Rz = torch.atan2(-transform[:, 2, 1] / Sy, transform[:, 2, 2] / Sz)
+    rotation_params = torch.stack([Rx, Ry, Rz], dim=1)
+
+    # Return parameters
+    return rotation_params, translation_params, scaling_params
