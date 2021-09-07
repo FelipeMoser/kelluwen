01159d1262d9e9cb762c35a831f2bd70fe7c13be
Felipe Moser <felipe.moser@univ.ox.ac.uk>
2021-09-07 18:59:45 +0100

    Added anisotropic_diffusion_3d

--- kelluwen/transforms.py
+++ kelluwen/transforms.py
@@ -1,5 +1,7 @@
 import torch
 import torch.nn.functional
+import numpy as np
+import math
 
 
 class AffineTransformer3d(torch.nn.Module):
@@ -98,7 +100,7 @@ def affine_transform_3d(rotation, translation, scaling):
         raise TypeError("scaling must be a tensor")
     elif scaling.dim() != 2 or scaling.shape[1] != 3:
         raise ValueError("scaling must be an Nx3 tensor")
-    
+
     # Calculate rotation transform
     R = rotation_transform_3d(rotation)
 
@@ -260,3 +262,120 @@ def parameters_from_homogeneous_affine_3d(transform):
 
     # Return parameters
     return rotation_params, translation_params, scaling_params
+
+
+def anisotropic_diffusion_3d(
+    input, iterations, delta_t=0.09, k=10, neighbours=26, mode=0
+):
+    """Returns input tensor after applying anisotropic diffusion filter
+    This algorithm is based on Perona et al. 1990: Scale-Space and Edge Detection Using Anisotropic Diffusion
+
+
+    Parameters
+    ----------
+    input : torch.Tensor
+        input tensor to be filtered, of shape (B, C, D, H, W)
+    iterations: int
+        number of times the anisotropic diffusion filter is applied to the tensor
+    delta_t : float
+        diffusion filter integration constant, should be less than 1/7, 1/11, or 1/14 when using 8, 18, or 26 neighbours, respectively; by default 0.09
+    k : float
+        diffusion constant; by default 10
+    neighbours : int
+        nearest neighbours used to calculate diffusion, by default 26
+    mode : int
+        diffusion function used, 0 and 1 are the examples offered in Perona et al. 1990by default 0
+
+
+    Returns
+    -------
+    torch.Tensor
+        filtered tensor
+    """
+    # Check input
+    if not isinstance(input, torch.Tensor):
+        raise TypeError("input must be a tensor")
+    elif input.dim() != 5:
+        raise ValueError("input must be a 5D tensor of shape (B, C, D, H, W)")
+    # Check iterations
+    if not isinstance(iterations, int):
+        raise TypeError("iterations must be an integer")
+    elif iterations <= 0:
+        raise ValueError("iterations must be a positive integer")
+    # Check delta_t
+    if not isinstance(delta_t, (int, float)):
+        raise TypeError("delta_t must be a number")
+    elif delta_t <= 0:
+        raise ValueError("delta_t must be a positive number")
+    # Check k
+    if not isinstance(k, (int, float)):
+        raise TypeError("k must be a number")
+    elif k <= 0:
+        raise ValueError("k must be a positive number")
+    # Check neighbours
+    if not isinstance(neighbours, (int)):
+        raise TypeError("neighbours must be an integer")
+    elif neighbours not in [6, 18, 26]:
+        raise ValueError("neighbours must be either 6, 18, or 26")
+    # Check mode
+    if not isinstance(mode, int):
+        raise TypeError("mode must be an integer")
+    elif mode not in [0, 1]:
+        raise ValueError("mode must be either 0 or 1")
+
+    # Define neighbour indices (index_i), their distances to node, and the node indices (index_0)
+    index_i = np.asarray(
+        [
+            [[None, -1], [None, None], [None, None]],  # North
+            [[None, None], [None, -1], [None, None]],  # West
+            [[None, None], [None, None], [None, -1]],  # Up
+            [[None, -1], [None, -1], [None, None]],  # North-west
+            [[None, -1], [1, None], [None, None]],  # North-east
+            [[None, -1], [None, None], [None, -1]],  # North-up
+            [[None, -1], [None, None], [1, None]],  # North-down
+            [[None, None], [None, -1], [None, -1]],  # West-up
+            [[None, None], [None, -1], [1, None]],  # West-down
+            [[None, -1], [None, -1], [None, -1]],  # North-west-up
+            [[None, -1], [None, -1], [1, None]],  # North-west-down
+            [[None, -1], [1, None], [None, -1]],  # North-east-up
+            [[None, -1], [1, None], [1, None]],  # North-east-down
+        ]
+    )
+    dist_i = np.asarray([1] * 3 + [math.sqrt(2)] * 6 + [math.sqrt(3)] * 4)
+    index_0 = np.flip(index_i.copy(), axis=2)
+    index_0[index_0 != None] *= -1
+
+    # Remove unwanted neighbours
+    if neighbours == 6:
+        filt = dist_i == 1
+    elif neighbours == 18:
+        filt = (dist_i == 1) + (dist_i == math.sqrt(2))
+    else:
+        filt = (dist_i == 1) + (dist_i == math.sqrt(2)) + (dist_i == math.sqrt(3))
+    index_i = index_i[filt]
+    dist_i = dist_i[filt]
+    index_0 = index_0[filt]
+
+    # Begin filtering
+    for _ in range(iterations):
+        for idx_i, d_i, idx_0 in zip(index_i, dist_i, index_0):
+            xi, yi, zi = idx_i
+            x0, y0, z0 = idx_0
+
+            # Calculate difference
+            diff = (
+                input[:, :, xi[0] : xi[1], yi[0] : yi[1], zi[0] : zi[1]]
+                - input[:, :, x0[0] : x0[1], y0[0] : y0[1], z0[0] : z0[1]]
+            )
+
+            # Calculate the flow contribution
+            if mode == 0:
+                flow = delta_t * torch.exp(-((diff / k) ** 2)) * diff / d_i
+            else:
+                flow = delta_t * 1 / (1 + (diff / k) ** 2) * diff / d_i
+
+            # Update image
+            input[:, :, x0[0] : x0[1], y0[0] : y0[1], z0[0] : z0[1]] += flow
+            input[:, :, xi[0] : xi[1], yi[0] : yi[1], zi[0] : zi[1]] -= flow
+
+    return input
