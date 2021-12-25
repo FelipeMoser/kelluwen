94a6c4ce1486a6d353043cfb6bf7ee6d2e432d64
Felipe Moser <felipe.moser@univ.ox.ac.uk>
2021-12-25 15:24:24 +0000

    Version v0.0.1-alpha

---
+++ README.md
@@ -0,0 +1,44 @@
+# Kelluwen
+
+Open AI library for research and education designed to be easy to use and develop with.
+
+## Installation
+
+```python
+pip install kelluwen
+```
+
+## Usage
+
+```python
+from kelluwen.functions.transforms import generate_affine, apply_affine
+from kelluwen.functions.tools import show_midplanes
+from torch import rand
+
+# Create image tensor of shape BxCxDxHxW
+img = rand(2, 3, 100, 100, 100)
+
+# Define parameters for affine transform. These transforms can be independent for each channel. If there is no channel dimension, the same transform will be applied for all channels.
+parameters = dict(parameter_translation=,
+    parameter_rotation=rand(2,3),
+    parameter_scaling=rand(2,3),
+    type_rotation="euler_xyz",
+    transform_order="trs",
+    type_output="dict",
+)
+
+# Generate affine transform
+transform = generate_affine(**parameters)
+
+# Apply affine transform to image tensor
+img_transformed = apply_affine(**transform)["image"]
+
+
+# Show midplanes of volume. In this example we're using the RAS coordinate system, and we're scaling the features using a min-max method.
+show_midplanes(
+    image=img_transformed,
+    title="Example midplanes",
+    show=True,
+    type_coordinates="ras",
+    type_scaling="min_max")
+# ```
---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kelluwen_development",
+    version="0.0.1",
+    description="Open AI library for research and education.",
+    packages=find_packages(),
+    url="https://github.com:FelipeMoser/kelluwen.git",
+    author="Felipe Moser",
+    author_email="felipe.moser@univ.ox.ac.uk",
+    install_requires=["torch>=1.10", "nibabel>=3.2", "matplotlib>=3.5"],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Operating System :: OS Independent",
+        "Environment :: GPU :: NVIDIA CUDA",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "License :: Attribution-ShareAlike 4.0 International (CC BY-SA 4.0) ",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "Topic :: Scientific/Engineering :: Image Processing",
+        "Topic :: Scientific/Engineering :: Medical Science Apps.",
+    ],
+    license_files=("LICENSE.txt"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
