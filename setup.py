433fb3ddf3b77af0b9ce486732f68cfa78716a4d
Felipe <felipe.moser@univ.ox.ac.uk>
2022-07-25 11:13:03 +0100

    v0.0.21

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kelluwen",
-    version="0.0.20",
+    version="0.0.21",
     description="Open AI library for research and education.",
     packages=find_packages(),
     url="https://github.com/felipemoser/kelluwen",
